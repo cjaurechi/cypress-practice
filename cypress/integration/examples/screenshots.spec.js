2fb680d3bf0cc492bc7bcb941895be5e39276786
cjaurechi <cjaurechi@asapp.com>
2021-04-22 20:55:41 -0300

    Adding screenshot tests

---
+++ cypress/integration/examples/screenshots.spec.js
@@ -0,0 +1,13 @@
+/// <reference types="Cypress" />
+
+describe('Screenshot tests', () => {
+    it('Full page screenshot', () => {
+        cy.visit('https://devexpress.github.io/testcafe/example/');
+        cy.screenshot({ capture: 'fullPage' });
+    })
+
+    it('Single element screenshot', () => {
+        cy.get('header').screenshot();
+        cy.get('#populate').screenshot();
+    })
+})

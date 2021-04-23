c768a6a54aeb3335814e76e1bf30243de2695aed
cjaurechi <cjaurechi@asapp.com>
2021-04-22 22:30:24 -0300

    Adding read/verify browser document content test

--- cypress/integration/examples/data.spec.js
+++ cypress/integration/examples/data.spec.js
@@ -16,4 +16,10 @@ describe('Write and read data to JSON / Text file', () => {
     it('Should read and verify data from Text file', () => {
         cy.readFile('log.txt').should('eql', 'Hello world!');
     });
+
+    it('Should read and verify browser document content', () => {
+        cy.visit('http://example.com');
+        cy.document().its('contentType').should('eq', 'text/html');
+        cy.document().should('have.property', 'charset').and('eq', 'UTF-8');
+    })
 })

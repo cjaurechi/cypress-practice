5421d484418b96e6b2600f8b722363f92c12c863
cjaurechi <cjaurechi@asapp.com>
2021-04-22 22:25:02 -0300

    Adding reading/writing json/text files tests

---
+++ cypress/integration/examples/data.spec.js
@@ -0,0 +1,19 @@
+/// <reference types="Cypress" />
+
+describe('Write and read data to JSON / Text file', () => {
+    it('Should write data into JSON', () => {
+        cy.writeFile('log.json', { name: "Charlie", age: 31 });
+    });
+
+    it('Should write data into Text file', () => {
+        cy.writeFile('log.txt', 'Hello world!');
+    });
+
+    it('Should read and verify data from JSON', () => {
+        cy.readFile('log.json').its('age').should('eq', 31);
+    });
+
+    it('Should read and verify data from Text file', () => {
+        cy.readFile('log.txt').should('eql', 'Hello world!');
+    });
+})

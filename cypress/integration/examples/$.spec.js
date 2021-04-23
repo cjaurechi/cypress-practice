7bc7ccd998bcd4539a8d36db585cf1690e6ebd62
cjaurechi <cjaurechi@asapp.com>
2021-04-22 23:20:06 -0300

    Testing Cypress.$ function

---
+++ cypress/integration/examples/$.spec.js
@@ -0,0 +1,13 @@
+/// <reference types="Cypress" />
+
+describe('Using Cypress.$ function', () => {
+    before(() => {
+        cy.visit('http://zero.webappsecurity.com')
+    })
+
+    it('Expose jQuery element in the current window', () => {
+        const signInButton = Cypress.$('#signin_button'); // Don't do this, it's a really bad practice
+        // signInButton.click();
+        signInButton.trigger('click');
+    })
+})
